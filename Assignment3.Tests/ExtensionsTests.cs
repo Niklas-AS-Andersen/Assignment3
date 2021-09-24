a6d0ecabd77e2472a72e29ba731250d72662bf5b
Niklas Aaron Sherman Andersen <naaa@itu.dk>
2021-09-24 13:17:24 +0200

    first done

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BDSA2020.Assignment02.Tests
@@ -6,9 +7,67 @@ namespace BDSA2020.Assignment02.Tests
     public class ExtensionsTests
     {
         [Fact]
-        public void Test1()
+        public void Flatten_given_two_list_returns_one_list()
         {
+            var list1 = new List<int>(){1,2,2,3,4,5};
+            var list2 = new List<int>(){4,6,8,4,2,7};
 
+            var collected = new List<List<int>>(){list1, list2};
+
+            Assert.Equal(collected.Flatten(), new List<int>(){1,2,2,3,4,5,4,6,8,4,2,7});
+        }
+
+        [Fact]
+        public void Filter_given_list_and_predicate_fortytwo_returns_correct()
+        {
+            var list = new List<int>(){1,4,7,9,3,5628,87,77,42,144};
+
+            Assert.Equal(list.Filter(fortytwoModuloSeven), new List<int>(){5628,77});
+        }
+
+        [Fact]
+        public void Filter_given_list_and_predicate_returns_leapyears()
+        {
+            var list = new List<int>(){2012,2013,2014,2015,2016,2017,2018,2019,2020,2021};
+
+            Assert.Equal(list.Filter(leapyear), new List<int>(){2012,2016,2020});
+        }
+
+        [Fact]
+        public void IsSecure_given_uri_returns_true()
+        {
+         var uri = new Uri("https://learnit.itu.dk/");
+
+        Assert.True(uri.IsSecure());
+        }
+
+        [Fact]
+        public void IsSecure_given_uri_returns_false()
+        {
+         var uri = new Uri("http://learnit.itu.dk/");
+
+        Assert.False(uri.IsSecure());
+        }
+
+        [Fact]
+        public void WordCount_given_sentence_returns_six()
+        {
+        var sentence = "Hello world, my name is Bob!";
+
+        Assert.Equal(6, sentence.WordCount());
+        }
+
+
+
+        //Predicate methods
+        public bool fortytwoModuloSeven(int number)
+        {
+            return (number > 42 && number % 7 == 0) ? true : false;
+        }
+
+        public bool leapyear(int year)
+        {
+            return DateTime.IsLeapYear(year);
         }
     }
 }
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,49 @@
 using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace BDSA2020.Assignment02
 {
     public static class Extensions
     {
+        public static IEnumerable<T> Flatten<T> (this IEnumerable<IEnumerable<T>> items) 
+        {
+            foreach (var item in items)
+            {
+                foreach (var it in item)
+                {
+                    yield return it;
+                }
+            }
+        }
+
+        public static IEnumerable<T> Filter<T> (this IEnumerable<T> items, Predicate<T> predicate)
+        {
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static bool IsSecure(this Uri uri)
+        {
+            return uri.Scheme.Equals("https");
+        }
+
+        public static int WordCount(this string str)
+        {
+            /* char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+
+            // var reg = new Regex("\\P{L}");
+
+            return str.Split(delimiterChars).Length; */
+
+            return str.Split(
+                new char[] {' ', ',', '.', '?', '!'},
+                StringSplitOptions.RemoveEmptyEntries).Length;
+        }
     }
 }
